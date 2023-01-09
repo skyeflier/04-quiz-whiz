1e6a5d4b6d1bf53048912b5de84a0975b7b2c8f4
skyeflier <119133572+skyeflier@users.noreply.github.com>
2023-01-09 16:12:27 -0600

    general commit

--- assets/js/score.js
+++ assets/js/score.js
@@ -2,7 +2,15 @@ const username = document.getElementById("username");
 const saveScoreBtn = document.getElementById("saveScoreBtn");
 const finalScore = document.getElementById("mostRecentScore");
 const mostRecentScore = localStorage.getItem("mostRecentScore");
-finalScore.textContent = mostRecentScore;
+
+const highScores = JSON.parse(localStorage.setItem("highScore")) || [];
+
+const maxScore = 3;
+
+console.log(highScores);
+
+console.log(JSON.parse(localStorage.getItem("highScores")));
+finalScore.innerText = mostRecentScore;
 
 username.addEventListener("keyup", () => {
     // console.log(username.value);
@@ -12,4 +20,15 @@ username.addEventListener("keyup", () => {
 saveHighScore = e => {
     console.log("clicked the save button!");
     e.preventDefault();
+
+    const score = {
+        score: mostRecentScore,
+        name: username.value
+    };
+    highScores.push(score);
+    highScores.sort((a, b) => b.score - a.score); // this is telling it if b is higher than a, put it above a (higher score)
+    highScores.splice(3); // This is keeping the top 3 scores
+
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    window.location.assign('/'); //WHAT IS THIS DOING??
 };
